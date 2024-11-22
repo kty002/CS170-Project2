5ec19f9c52ce34b80ffca9a0d4415a449fbb2e43
codingwitharhum <107953334+codingwitharhum@users.noreply.github.com>
2024-11-21 19:57:51 -0800

    Update Problem.cpp with custom algorithm

--- Problem.cpp
+++ Problem.cpp
@@ -116,6 +116,85 @@ void Problem::backward_elimination(int total_features) {
 
 //ad custom algorithm
 void Problem::custom_algorithm(int total_features) {
-    std::cout << "Beginning Custom Algorithm...\n";
-    return;
+    std::cout << "Beginning Custom Algorithm (Bi-Directional Search)...\n\n";
+
+    // Initialize forward and backward sets
+    std::vector<int> forward_set;
+    std::vector<int> backward_set;
+    for (int feature = 1; feature <= total_features; ++feature) {
+        backward_set.push_back(feature);
+    }
+
+    double forward_score = 0.0;
+    double backward_score = evaluate(backward_set);
+
+    std::cout << "Initial backward set " << Node(backward_set).to_string()
+              << " accuracy is " << backward_score << "%\n\n";
+
+    // Bi-directional search
+    while (true) {
+        // Add the best feature to forward_set
+        Node best_forward({}, -std::numeric_limits<double>::infinity());
+        for (int feature = 1; feature <= total_features; ++feature) {
+            if (std::find(forward_set.begin(), forward_set.end(), feature) != forward_set.end()) {
+                continue; // Skip features that are already in the forward set
+            }
+
+            std::vector<int> new_set = forward_set;
+            new_set.push_back(feature);
+            double score = evaluate(new_set);
+
+            std::cout << "  Forward step: Using feature(s) " << Node(new_set).to_string()
+                      << " accuracy is " << score << "%\n";
+
+            if (score > best_forward.score) {
+                best_forward = Node(new_set, score);
+            }
+        }
+
+        if (best_forward.score > forward_score) {
+            forward_set = best_forward.features;
+            forward_score = best_forward.score;
+            std::cout << "  Forward: Feature set " << best_forward.to_string()
+                      << " was best, accuracy is " << best_forward.score << "%\n\n";
+        }
+
+        // Remove the least important feature from backward_set
+        Node best_backward({}, -std::numeric_limits<double>::infinity());
+        for (int feature : backward_set) {
+            std::vector<int> new_set = backward_set;
+            new_set.erase(std::remove(new_set.begin(), new_set.end(), feature), new_set.end());
+            double score = evaluate(new_set);
+
+            std::cout << "  Backward step: Using feature(s) " << Node(new_set).to_string()
+                      << " accuracy is " << score << "%\n";
+
+            if (score > best_backward.score) {
+                best_backward = Node(new_set, score);
+            }
+        }
+
+        if (best_backward.score > backward_score) {
+            backward_set = best_backward.features;
+            backward_score = best_backward.score;
+            std::cout << "  Backward: Feature set " << best_backward.to_string()
+                      << " was best, accuracy is " << best_backward.score << "%\n\n";
+        }
+
+        // Checking the convergence
+        if (forward_set == backward_set) {
+            std::cout << "Forward and Backward searches have converged!\n";
+            break;
+        }
+
+        // Stopping if there is no improvement in either direction
+        if (best_forward.score <= forward_score && best_backward.score <= backward_score) {
+            std::cout << "(Warning: No improvement in both directions!)\n";
+            break;
+        }
+    }
+
+    // Displaying final output
+    std::cout << "Finished Bi-Directional Search!! The best feature subset is " << Node(forward_set).to_string()
+              << ", which has an accuracy of " << forward_score << "%\n";
 }
